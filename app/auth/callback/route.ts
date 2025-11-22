ef176e6706e3f34d0092d39285942a76af22f489
mhardiansyah <mhardiansyah5766@gmail.com>
2025-11-22 20:50:18 +0700

    test

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,16 +2,27 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const code = url.searchParams.get("code");
 
+  const supabase = createRouteHandlerClient(
+    {
+      cookies,
+    },
+    {
+      supabaseUrl: process.env.SUPABASE_URL!,
+      supabaseKey: process.env.SUPABASE_ANON_KEY!,
+    }
+  );
+
   if (code) {
-    const supabase = createRouteHandlerClient({ cookies });
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (error) {
-      console.log(error);
+      console.error(error);
       return NextResponse.redirect("/login?error=session");
     }
   }
