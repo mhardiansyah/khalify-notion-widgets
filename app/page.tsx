2d7aa16d9b67620211d2a623b3bca3e374b20da1
mhardiansyah <mhardiansyah5766@gmail.com>
2025-12-18 16:15:59 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import LoginPage from "./auth/login/page";
 
 export default function Home() {
   return (
-    <div className="">
-      <p>Hello</p>
-    </div>
+    <>  
+    <LoginPage />
+    </>
   );
 }
