b49af0489d811e178e12f5330a9462ce4d7c4165
mhardiansyah <mhardiansyah5766@gmail.com>
2025-12-18 13:58:50 +0700

    test

--- app/embed/[id]/page.tsx
+++ app/embed/[id]/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+export const dynamic = "force-dynamic";
 
 import ClientViewComponent from "@/app/components/ClientViewComponent";
 import { queryDatabase } from "@/app/lib/notion-server";
