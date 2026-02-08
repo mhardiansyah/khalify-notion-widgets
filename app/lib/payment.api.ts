6e3e4ccc61827ef556b25fcf0f79080bfac7d18b
mhardiansyah <mhardiansyah5766@gmail.com>
2026-02-09 02:11:59 +0700

    chore: add gitignore and fix backend structure

--- app/lib/payment.api.ts
+++ app/lib/payment.api.ts
@@ -1,21 +1,31 @@
 // lib/payment.api.ts
-import { headers } from "next/headers";
 import { api } from "./axios";
+import Cookies from "js-cookie";
 
 export const getPaymentLink = async () => {
-  const res = await api.post("/payment/link", {
-    headers: {
-      Authorization: `Bearer ${(await headers()).get("login_token")}`,
-    },
-  });
+  const token = Cookies.get("login_token");
+
+  const res = await api.post(
+    "/payment/link",
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
   return res.data;
 };
 
 export const checkPaymentStatus = async () => {
+  const token = Cookies.get("login_token");
+
   const res = await api.get("/payment/check-status", {
     headers: {
-      Authorization: `Bearer ${(await headers()).get("login_token")}`,
+      Authorization: `Bearer ${token}`,
     },
   });
+
   return res.data;
 };
