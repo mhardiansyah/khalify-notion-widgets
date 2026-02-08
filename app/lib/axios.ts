15f93d95c8e1e791e70c05c18f1c18e0793fe346
mhardiansyah <mhardiansyah5766@gmail.com>
2026-02-09 02:27:58 +0700

    chore: add gitignore and fix backend structure

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -9,12 +9,16 @@ export const api = axios.create({
     },
 });
 
-// Logic Interceptor: Biar semua request otomatis bawa token
+// Tambahkan interceptor ini agar setiap request membawa token terbaru
 api.interceptors.request.use((config) => {
+    // Ambil token dari cookie 'login_token'
     const token = Cookies.get("login_token");
+    
     if (token) {
+        // Tempelkan ke header Authorization seperti standar Postman
         config.headers.Authorization = `Bearer ${token}`;
     }
+    
     return config;
 }, (error) => {
     return Promise.reject(error);
--- app/lib/payment.api.ts
+++ app/lib/payment.api.ts
@@ -2,12 +2,13 @@
 import { api } from "./axios";
 
 export const getPaymentLink = async () => {
-  // Post data kosong {} karena BE cuma butuh info user dari token
-  const res = await api.post("/payment/link", {});
+  // Gunakan instance 'api' agar interceptor jalan
+  const res = await api.post("/payment/link", {}); 
   return res.data;
 };
 
 export const checkPaymentStatus = async () => {
+  // Gunakan instance 'api'
   const res = await api.get("/payment/check-status");
   return res.data;
 };
