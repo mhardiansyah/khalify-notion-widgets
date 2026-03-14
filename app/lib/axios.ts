fb1e36714997e06c3d7651bfdbf6d7390e75d355
‘Rafizi’ <arrafizinaufal@gmail.com>
2026-03-14 15:48:13 +0700

    flexibilty link be (local/vercel)

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -2,8 +2,10 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4545";
+
 export const api = axios.create({
-    baseURL: "http://localhost:4545", // Pastikan URL bener
+    baseURL: API_URL, 
     headers: {
         "Content-Type": "application/json",
     },
